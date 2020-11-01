6bf4628d6d05d43eec8d4f059b99dbd081b14457
gufimov <72704694+gufimov@users.noreply.github.com>
2020-11-01 13:27:22 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,5 +1,5 @@
 class Creds(object):
-    TOKEN = "1126750432:AAF5IwVsZuIWGQ-rIcFPmiecE1OGSDbt7cQ"
+    TOKEN = "1401959235:AAFhvCLEeic_Ga7_JfOCTpNXU8hbZZF_iA0"
     CLIENT_ID = "client id from google cloud console"
     CLIENT_SECRET = "client secret from google cloud console"
     PATH_ID = "folder id of the gdrive folder in which the bot will upload its files"
